9d3d364f835a9f91e5c3139bd5601c674de816ae
Dobi Tamas <dobi.tamas0428@gmail.com>
2020-11-23 15:09:06 +0100

    App.js shows all the components and gets updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+
+
+import {Cards, Chart, CountryPicker} from './components'
+import styles from './App.module.css'
+import {fetchData} from './api';
+
 
 class App extends React.Component {
-  render() {
-    const {data, country} = this.state;
+    state = {
+        data: {},
+        country: '',
+    }
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+
+        this.setState({data: fetchedData})
+
+    }
+
+    handleCountryChange = async(country) => {
+        const fetchedData = await fetchData(country);
+
+        this.setState({data: fetchedData, country: country});
+
+    }
+
+    render(){
+        const {data, country} = this.state;
         return(
             <div className={styles.container}>
                 <Cards data={data}/>
@@ -11,7 +35,7 @@ class App extends React.Component {
                 <Chart data={data} country={country}/>
             </div>
         )
-  }
+    }
 }
 
 export default App;
