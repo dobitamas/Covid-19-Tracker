ee0b3e12b67da1468f3237814db4a3acff80bacb
Dobi Tamas <dobi.tamas0428@gmail.com>
2020-11-23 15:04:07 +0100

    Created Card component with CSS

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 
 class App extends React.Component {
   render() {
-    return <div>Chart</div>
+    const {data, country} = this.state;
+        return(
+            <div className={styles.container}>
+                <Cards data={data}/>
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart data={data} country={country}/>
+            </div>
+        )
   }
 }
 
